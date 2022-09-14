55d0950cad4694cb69f387b28f459f656441871d
Teresa <luvmargaritas@gmail.com>
2022-09-14 11:12:16 -0500

    delete dash to push branch

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* ------------------------------------------------------------------------------------------------
+/* ---------------------------------------------------------------------------------------------
 
 CHALLENGE 1 - Review
 
@@ -15,7 +15,7 @@ const raisedToTheThird = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1. 
+Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1.
 ------------------------------------------------------------------------------------------------ */
 
 const addOne = (arr) => {
