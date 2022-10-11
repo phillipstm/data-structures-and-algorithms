82d70cf36dd2edd7ff343d613fb3cfd6b0c5c43c
Teresa <luvmargaritas@gmail.com>
2022-10-10 19:19:01 -0500

    added basic test????

---
+++ javascript/code-challenges/tree-max/treeMax.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const treeMax = require('./treeMax');
+
+describe('Tree Max', () => {
+  it('works', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+
+//Find the maximum value stored in the tree.
+//You can assume that the values stored in the Binary Tree will be numeric.
