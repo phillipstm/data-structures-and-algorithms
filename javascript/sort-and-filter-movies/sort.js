c12e6fd2a622640444ed06f7073c5674e7342446
Teresa <luvmargaritas@gmail.com>
2022-10-27 00:35:34 -0500

    tried GH no go

---
+++ javascript/sort-and-filter-movies/sort.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+// Implement the functions sortYear, sortTitle, and inGenre in the file sort.ts.
+const movies = require('./movies');
+
+////EXAMPLE FOR DEMO
+const books = ['A Tale of Two Cities', 'Mary Poppins', 'Lord of the Rings'];
+
+let sampleOperator
+
+
