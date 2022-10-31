1f7f1b49a29592951bfcc38004789549c20252d3
Teresa <luvmargaritas@gmail.com>
2022-10-31 00:29:39 -0500

    adding some content

--- javascript/hashtable/index.js
+++ javascript/hashtable/index.js
@@ -1,3 +1,47 @@
 'use strict';
 
+const { LinkedList } = require('..linked-list');
+const { get } = require('cheerio/lib/api/traversing');
+
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
+  }
+
+  hash(key) {
+    let characters = key.split('');
+    let asciiSum = characters.reduce((sum, character) => {
+      return sum + character.charCodeAt(0);
+    },0)
+
+    let initialHash = asciiSum * 599;
+    return initialHash % this.size;
+
+    set(key.value){
+      let position = this.hash(key);
+      let data = { [key]: value }
+
+      if(this.bucket[position])
+        let bucket = this.buckets[position];
+      bucket.add(data);
+    } else {
+      let bucket = new LinkedList();
+      bucket.add(data);
+      this.bucket[position] = buckets;
+      get(key){
+        let position = this.hash(key);
+        if (this.bucket[position][key]) {
+          let bucket = this.buckets[position];
+          let value = bucket.head.value[key]
+          return value
+        }
+      }
+    }
+  }
+}
+let
+  table = new HashTable(1024);
+console.log(table);
+console.log(table.hash('BiteMe'));
 
