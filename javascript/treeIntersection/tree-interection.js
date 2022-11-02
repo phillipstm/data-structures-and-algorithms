d660eacc5de13f4f468154c2a999ed135065bced
Teresa <luvmargaritas@gmail.com>
2022-11-01 23:12:01 -0500

    adding file

---
+++ javascript/treeIntersection/tree-interection.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { HashTable } = require('../hashtable/index');
+const { get } = require('cheerio/lib/api/traversing');
+const { Node } = require('../linked-list/linkedlist');
+
+
+//Write a function called tree_intersection
+// that takes two binary trees as parameters
+//Using your Hashmap implementation as a part of your algorithm,
+//return a set of values found in both trees.
+
+//check for root node on each tree
+//traverse left
+//push values to []
+//traverse right
+//push values to [
+  //compare values
+  //push to new Array
+  //or compare values
+]
+function tree_intersection
