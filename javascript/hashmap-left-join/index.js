2522d8f42b14d5225e8f1aa8fc9d9abc9c8adfc7
Teresa <luvmargaritas@gmail.com>
2022-11-07 20:56:21 -0600

    added content

--- javascript/hashmap-left-join/index.js
+++ javascript/hashmap-left-join/index.js
@@ -7,3 +7,11 @@ import { HashTable } from require('../hashtable/index');
 const { LinkedList } = require('..linked-list');
 const { get } = require('cheerio/lib/api/traversing');
 const { HashTable } = require(../)
+
+
+//
+//function leftJoin
+//
+// takes 2 Hashmaps
+// joins matching in left HashTable
+// returns array that has matching left key
